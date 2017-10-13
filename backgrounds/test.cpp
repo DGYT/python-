cd3c1a3727a9d7d09fc4989e1ece91a9f857f2aa
黄紫奇 <787094470@qq.com>
2017-10-13 10:26:16 +0800

    final py

--- backgrounds/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <windows.h>
-
-void main()
-{
-    SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, "C:\\backgrounds\\morning.bmp", SPIF_SENDCHANGE);
-    SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, "C:\\backgrounds\\morning.bmp", SPIF_SENDCHANGE);
-    SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, "C:\\backgrounds\\morning.bmp", SPIF_SENDCHANGE);
-    
-}
--- backgrounds/v4.py
+++ backgrounds/v4.py
@@ -1,17 +1,24 @@
 import os
 import time
+from ctypes import *
 from winreg import *
 
-mor_path = 'C:\\backgrounds\morning.exe'
-day_path = 'C:\\backgrounds\day.exe'
-nig_path = 'C:\\backgrounds\\night.exe'
+dir = os.getcwd()
+mor_path = dir + '\morning.bmp'
+day_path = dir + '\day.bmp'
+nig_path = dir + '\\night.bmp'
 
 
 def change_desk(picpath):
     key = CreateKey(HKEY_CURRENT_USER, "Control Panel\Desktop")
     SetValueEx(key, "Wallpaper", 0, REG_SZ, picpath)
     CloseKey(key)
-    os.system(picpath)
+    change(picpath)
+
+
+def change(picpath):
+    lib = windll.LoadLibrary('USER32.DLL')
+    lib.SystemParametersInfoW(0x0014, 0, picpath, 0x0002);
 
 
 def current_hour():
